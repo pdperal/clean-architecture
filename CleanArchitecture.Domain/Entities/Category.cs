14bd0c6c63adc4d2c0692894d77ec99fc1198eeb
Pedro Peral <pep.90@hotmail.com>
2021-04-30 14:33:52 -0300

    feature: add more Domain validations.

--- CleanArchitecture.Domain/Entities/Category.cs
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -1,9 +1,5 @@
 ﻿using CleanArchitecture.Domain.Validation;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Entities
 {
--- CleanArchitecture.Domain/Entities/Product.cs
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Domain.Validation;
 
 namespace CleanArchitecture.Domain.Entities
 {
@@ -16,5 +12,55 @@ namespace CleanArchitecture.Domain.Entities
         public string Image { get; private set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0,
+                "Invalid object state. Id is required.");
+
+            ValidateDomain(name, description, price, stock, image);
+
+            Id = id;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid object state. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid object state. Name requires at least 3 characteres.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Invalid object state. Name is required.");
+            
+            DomainExceptionValidation.When(description.Length < 5,
+                "Invalid object state. Description requires at least 5 characteres.");
+
+            DomainExceptionValidation.When(price < 0,
+                "Invalid object state. Price value is negative.");
+            
+            DomainExceptionValidation.When(stock < 0,
+                "Invalid object state. Stock quantity is negative.");
+
+            DomainExceptionValidation.When(image.Length > 250,
+                "Invalid object state. Image name has more then 250 characteres.");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
     }
 }
