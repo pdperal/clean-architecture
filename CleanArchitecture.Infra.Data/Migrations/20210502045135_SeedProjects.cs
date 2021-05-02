7add7d74262626fdae3c2a75c5a0345b680fedc0
Pedro Peral <pep.90@hotmail.com>
2021-05-02 02:04:57 -0300

    feature: add first migration.

---
+++ CleanArchitecture.Infra.Data/Migrations/20210502045135_SeedProjects.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infra.Data.Migrations
+{
+    public partial class SeedProjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name)" +
+                "Values('Material Escolar')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name)" +
+                "Values('Outros')");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "Values('Caderno espiral', 'Caderno Espiral 100 Folhas', 7.50, 50, 'caderno.jpg', 5)");
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "Values('Estojo', 'Estojo grande', 10, 50, 'estojo.jpg', 5)");
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "Values('Caneta', 'Caneta preta', 1, 10, 'caneta.jpg', 5)");
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "Values('Calculadora', 'Calculadora científica', 15, 5, 'calculadora.jpg', 6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
